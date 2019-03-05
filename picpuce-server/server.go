69168f9ae2364d341e1dd6d978366ab7c82733db
jroquelaure <jonathanr@jfrog.com>
2019-03-05 11:30:47 +0100

    change service id

--- picpuce-server/server.go
+++ picpuce-server/server.go
@@ -62,11 +62,11 @@ func (s *service) UploadRandomArtifacts(ctx context.Context, req *utils.Scenario
 	log.Println("_____ New Scenario Loaded _____")
 	srv := k8s.NewService(
 		// This name must match the package name given in your protobuf definition
-		micro.Name("picpuce.runner"),
+		micro.Name("go.micro.srv.runner"),
 	)
 	srv.Init()
 
-	client := pr.NewRunnerService("picpuce.runner", srv.Client())
+	client := pr.NewRunnerService("go.micro.srv.runner", srv.Client())
 
 	var binDesc = &BinDescription{minSize: req.MinSize, maxSize: req.MaxSize}
 
